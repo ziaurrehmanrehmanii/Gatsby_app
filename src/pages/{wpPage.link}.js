2d87c79aba26322db379fac8adf8e5bd68b6dd27
ziaurrehmanrehmani <ziaurrehmanrehmani@gmail.com>
2022-04-10 23:47:57 -0700

    Auto Commit

--- src/pages/{wpPage.link}.js
+++ src/pages/{wpPage.link}.js
@@ -1,11 +1,33 @@
 import React from "react"
+import { graphql } from "gatsby"
+import styled from "styled-components"
+//components
+import Layout from "../components/Layout/Layout"
 
-const PageTemplate = () =>(
-    <div>
-        <p>
-            Page
-        </p>
-    </div>
+const Wrapper = styled.div`
+  max-width: 1180px;
+  margin: 0 auto;
+  padding: 20px;
+`
+
+const ContentWrapper = styled.div`
+  display: block;
+  @media (min-width: 992px) {
+    display: flex;
+  }
+`
+const PageContent = styled.article`
+  margin-top: 20px;
+`
+
+const PageTemplate = () => (
+  <Layout>
+    <p>PageHero</p>
+    <Wrapper>
+      <p>Sidebar</p>
+      <p>Content</p>
+    </Wrapper>
+  </Layout>
 )
 
 export default PageTemplate
